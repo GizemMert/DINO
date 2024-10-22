23812b9ae1f810869e7ac18365c8f8cbb6598896
GizemMert <gizem.mert12@gmail.com>
2024-10-23 00:39:23 +0200

    updated

--- create_artificial_patients/combined_uncertain_fold4.py
+++ create_artificial_patients/combined_uncertain_fold4.py
@@ -15,6 +15,9 @@ import numpy as np
 import pickle
 from transformers import ViTFeatureExtractor
 from PIL import Image
+import types
+dinov2 = types.ModuleType("dinov2")
+sys.modules["dinov2"] = dinov2
 
 feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
 
